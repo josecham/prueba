35ff0429e2e7c5a9e5afc86fa906780b497421f3
josecham <gabriel_influencia74@hotmail.com>
2023-01-25 23:38:49 -0300

    la etiqueta de lo que guardo

--- form.php
+++ form.php
@@ -1,17 +1,17 @@
 <?php
-$nombre = $_POST ['nombre'];
-$email = $_POST ['email'];
+$nombre = $_POST ["name"];
+$email = $_POST ["email"];
 
 $http_response_header .= "Content-Type: text/pain";
 
 $mensaje = "este mensaje fue enviado por" . $nombre . ",\r\n";
 $mensaje = "su email es" . $email . ",\r\n";
 
-$para = 'gabriel_influencia74@hotmail.com';
-$asunto = 'mensaje de mi sitio web';
+$para = "gabriel_influencia74@hotmail.com";
+$asunto = "mensaje de mi sitio web";
 
 mail($para, $asunto, ultf8_decore($mensaje), $http_response_header);
 
-header (location:'index.html');
+header (location:"index.html");
 
 ?>
