72e4d8fa91a1b2f3d033af82e468293ec19ce1b3
josecham <gabriel_influencia74@hotmail.com>
2023-01-27 10:43:24 -0300

    la etiqueta de lo que guardo

---
+++ formulario.html/procesar.php
@@ -0,0 +1,14 @@
+<?php
+$nombre = $_POST ['nombre'];
+$email = $_POST ['email'];
+
+$mensaje = "este mensaje fue enviado por " . $nombre . " \r\n";
+$mensaje = "su email es: " . $email . " \r\n";
+
+$para = 'gabriel_influencia74@hotmail.com';
+$asunto = 'mensaje de mi sitio web';
+
+mail($para, $asunto, ultf8_decore($mensaje), $header);
+
+header (location:'formula.html');
+?>
