64dec4d66d5f97fa3a4068d7cf275231c15aa193
josecham <gabriel_influencia74@hotmail.com>
2023-01-28 00:21:40 -0300

    la etiqueta de lo que guardo

--- formulario.html/procesar.php
+++ formulario.html/procesar.php
@@ -1,14 +1,24 @@
 <?php
-$nombre = $_POST ['nombre'];
-$email = $_POST ['email'];
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-$mensaje = "este mensaje fue enviado por " . $nombre . " \r\n";
-$mensaje = "su email es: " . $email . " \r\n";
+$header = 'From: ' . $mail . " \r\n";
+$header. = "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header. = "Mime-Version: 1.0 \r\n";
+$header. = "Content-Type: text/plain";
+
+$message = "este mensaje fue enviado por: " . $name . " \r\n";
+$message = "su e-mail es: " . $mail . " \r\n";
+$message = "telefono de contacto: " . $phone . " \r\n";
+$message = "mensaje: " . $_POST ['message'] . " \r\n";
+$message = "enviado el: " . date('d/m/Y', time());
 
 $para = 'gabriel_influencia74@hotmail.com';
-$asunto = 'mensaje de mi sitio web';
+$asunto = 'asunto del mensaje';
 
-mail($para, $asunto, ultf8_decore($mensaje), $header);
+mail($para, $asunto, ultf8_decode($message), $header);
 
-header (location:'formula.html');
+header ("Location:formula.html");
 ?>
